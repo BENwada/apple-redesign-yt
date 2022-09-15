23f3d737880d5e8eda98df6a69c6f0863a295485
BENwada <domo0103wada@gmail.com>
2022-09-15 20:51:46 +0900

    landing added

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,11 +4,13 @@ import React from "react";
 
 import {
   MagnifyingGlassIcon,
-  BriefcaseIcon,
+  ShoppingBagIcon,
   UserIcon,
 } from "@heroicons/react/24/outline";
 
 const Header = () => {
+  const session = false;
+
   return (
     <header className="sticky top-0 z-30 flex w-full items-center justify-between bg-[#e7ecee] p-4">
       <div className="flex items-center justify-center md:w-1/5">
@@ -37,9 +39,35 @@ const Header = () => {
           Business
         </a>
       </div>
-      <div className="">
+      <div className="flex items-center justify-center gap-x-4 md:w-1/5">
         <MagnifyingGlassIcon className="headerIcon" />
-        <div></div>
+        <Link href="/checkout">
+          <div className="relative cursor-pointer">
+            <span className="absolute -top-1 -right-1 z-50 flex h-4 w-4 items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500 text-[10px] text-white">
+              5
+            </span>
+            <ShoppingBagIcon className="headerIcon" />
+          </div>
+        </Link>
+
+        {session ? (
+          <Image
+            src={
+              // session.user?.image ||
+              "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y"
+            }
+            alt=""
+            className="cursor-pointer rounded-full"
+            width={34}
+            height={34}
+            // onClick={() => signOut()}
+          />
+        ) : (
+          <UserIcon
+            className="headerIcon"
+            // onClick={() => signIn()}
+          />
+        )}
       </div>
     </header>
   );
