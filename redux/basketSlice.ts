bdb478d466a09111daecca0298c560ebfac7b852
BENwada <domo0103wada@gmail.com>
2022-09-28 18:45:27 +0900

    basketSlice add

--- redux/basketSlice.ts
+++ redux/basketSlice.ts
@@ -45,6 +45,15 @@ export const basketSlice = createSlice({
 export const { addToBasket, removeFromBasket } = basketSlice.actions;
 
 //Selectors -> retrieving items in state to use in different components
-export const selectorsBasketItems = (state: RootState) => state.basket.items;
+export const selectBasketItems = (state: RootState) => state.basket.items;
+export const selectBasketItemsWithId = (state: RootState, id: string) => {
+  state.basket.items.filter((item: Product) => item._id === id);
+};
+export const selectBasketTotal = (state: RootState) => {
+  state.basket.items.reduce(
+    (total: number, item: Product) => (total += item.price),
+    0
+  );
+};
 
 export default basketSlice.reducer;
